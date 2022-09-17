b3823e105caaa966645d0eb45cf254dfea2abf4b
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-09-17 17:36:04 -0300

    Desafio vetor

--- Exercício-DesafioVetor/src/Imovel.java
+++ Exercício-DesafioVetor/src/Imovel.java
@@ -1,4 +1,25 @@
-
 public class Imovel {
-
+	private String endereco;
+	private Locatario locatario;
+	private Proposta[] proposta;
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Locatario getLocatario() {
+		return locatario;
+	}
+	public void setLocatario(Locatario locatario) {
+		this.locatario = locatario;
+	}
+	public Proposta[] getProposta() {
+		return proposta;
+	}
+	public void setProposta(Proposta[] proposta) {
+		this.proposta = proposta;
+	}
+	
 }
