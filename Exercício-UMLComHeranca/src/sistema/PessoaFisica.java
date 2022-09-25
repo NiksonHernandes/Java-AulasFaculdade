0f7a88d7e6a1af6db7f270827f27021d319c2e2b
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-09-25 14:10:07 -0300

    ExercíciosJavaPOO

---
+++ Exercício-UMLComHeranca/src/sistema/PessoaFisica.java
@@ -0,0 +1,21 @@
+package sistema;
+
+public class PessoaFisica extends Pessoa{
+	private int cpf;
+	private String dataNasci;
+	
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getDataNasci() {
+		return dataNasci;
+	}
+	public void setDataNasci(String dataNasci) {
+		this.dataNasci = dataNasci;
+	}
+	
+	
+}
---
+++ Exercício-UMLComHeranca/src/sistema/PessoaJuridica.java
@@ -0,0 +1,14 @@
+package sistema;
+
+public class PessoaJuridica {
+	private int cnpj;
+
+	public int getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(int cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+}
