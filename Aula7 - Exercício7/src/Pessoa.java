52bd430f81d2c073534d04e24c0dc85e39b5974d
Nikson Hernandes <Nik.Hernandes14@hotmail.com>
2022-09-26 21:31:08 -0300

    ExercíciosJavaPOO

---
+++ Aula7 - Exercício7/src/Pessoa.java
@@ -0,0 +1,33 @@
+
+public class Pessoa {
+	private String nome;
+	private Pessoa conjuge;
+	private Pessoa mae;
+	private Pessoa pai;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Pessoa getConjuge() {
+		return conjuge;
+	}
+	public void setConjuge(Pessoa conjuge) {
+		this.conjuge = conjuge;
+	}
+	public Pessoa getMae() {
+		return mae;
+	}
+	public void setMae(Pessoa mae) {
+		this.mae = mae;
+	}
+	public Pessoa getPai() {
+		return pai;
+	}
+	public void setPai(Pessoa pai) {
+		this.pai = pai;
+	}
+	
+}
